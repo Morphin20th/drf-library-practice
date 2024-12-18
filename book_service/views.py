e618bfa632175fc34f2234c6d9a22ff916b06a53
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-12-18 22:32:30 +0200

    feat permissions for book service

--- book_service/views.py
+++ book_service/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly
 
 from book_service.models import Book
 from book_service.serializers import BookSerializer
@@ -7,3 +8,10 @@ from book_service.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action == "list":
+            return [AllowAny()]
+        elif self.action in ("retrieve", "update", "partial_update", "destroy"):
+            return [IsAdminUser()]
+        return [IsAuthenticatedOrReadOnly()]
