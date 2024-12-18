c1eae880b842f10ff58a88cb2da6ecff8b604f44
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-12-18 19:33:16 +0200

    add new model User

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "book_service",
-    "user"
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -118,3 +118,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
