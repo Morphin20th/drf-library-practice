2c9beb2dacecffe3201740f74860c7ce369fe5a2
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-12-18 20:08:14 +0200

    feat JWT authentication by default

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 from user.views import CreateUserView
 
 urlpatterns = [
     path("", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_get"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 app_name = "user"
