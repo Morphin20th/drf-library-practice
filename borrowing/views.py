adad7ef0c5c81e7d8e3cdfb7b46e2b3ff78bf863
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-12-19 00:04:49 +0200

    implement Borrowing list endpoint

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingListSerializer, BorrowingSerializer
+
+
+class BorrowingViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        return BorrowingSerializer
