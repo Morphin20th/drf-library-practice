9bd80716038774bbe1de410c39d60e8acd654981
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-12-18 20:14:27 +0200

    feat ManageUserView

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_get"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
