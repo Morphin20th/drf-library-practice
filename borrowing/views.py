43aecc4d2f4a2a1514f372476170cfbf859f2516
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-12-19 19:26:42 +0200

    register Borrowing model in admin panel, configure queryset, fix N+1 problem

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
@@ -16,6 +17,7 @@ class BorrowingViewSet(
     mixins.CreateModelMixin,
 ):
     queryset = Borrowing.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -25,3 +27,9 @@ class BorrowingViewSet(
         if self.action == "create":
             return BorrowingCreateSerializer
         return BorrowingSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset.select_related()
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(user=self.request.user)
