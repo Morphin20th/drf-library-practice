08b9494b97d3e5ca16d8c4b767e5033b3050a0fb
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-12-19 15:49:58 +0200

    add validate_borrowing staticmethod, is_active field

--- borrowing/models.py
+++ borrowing/models.py
@@ -14,20 +14,58 @@ class Borrowing(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
     )
+    is_active = models.BooleanField(default=True)
 
-    def clean(self):
-        if self.expected_return_date <= self.borrow_date:
-            raise ValidationError(
+    @staticmethod
+    def validate_borrowing(
+        expected_return_date,
+        borrow_date,
+        actual_return_date,
+        book,
+        user,
+        error_to_raise,
+    ):
+        if expected_return_date <= borrow_date:
+            raise error_to_raise(
                 {
                     "expected_return_date": "Expected return date must be later than the borrow date."
                 }
             )
-        if self.actual_return_date <= self.borrow_date:
-            raise ValidationError(
+        if actual_return_date <= borrow_date:
+            raise error_to_raise(
                 {
                     "actual_return_date": "Actual return date must be later than the borrow date."
                 }
             )
+        if book.inventory == 0:
+            raise error_to_raise({"book.inventory": "Inventory must be more than 0"})
+        if user.borrowings.filter(is_active=True).count() == 1:
+            raise error_to_raise({"user": "User already has an active borrowing."})
+
+    def clean(self):
+        self.validate_borrowing(
+            expected_return_date=self.expected_return_date,
+            borrow_date=self.borrow_date,
+            actual_return_date=self.actual_return_date,
+            book=self.book,
+            user=self.user,
+            error_to_raise=ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        if self.actual_return_date:
+            self.is_active = False
+
+        self.full_clean()
+        return super(Borrowing, self).save(
+            force_insert, force_update, using, update_fields
+        )
 
     class Meta:
         constraints = [
