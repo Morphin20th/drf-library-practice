14ccf7cdcf5979cf11010a4bf9d6c4400eaf0657
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-12-19 14:07:33 +0200

    update Borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 from book_service.models import Book
+from user.models import User
 
 
 class Borrowing(models.Model):
@@ -9,6 +10,7 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def clean(self):
         if self.expected_return_date <= self.borrow_date:
